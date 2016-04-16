ad7fa75949aee75fd66374732ae167aa952eafd3
Liang Tang <liang.xjtu@gmail.com>
2016-04-16 16:08:45 -0500

    debugging drag issues

--- Happiness/HappinessUIViewController.swift
+++ Happiness/HappinessUIViewController.swift
@@ -13,8 +13,30 @@ class HappinessUIViewController: UIViewController, FaceViewDataSource {
     @IBOutlet weak var faceView: FaceView!{
         didSet{
             faceView.dataSource = self;
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
         }
     }
+    
+    private struct Constants{
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture:UIPanGestureRecognizer) {
+        switch gesture.state{
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView);
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0{
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView);
+            }
+        
+        default: break;
+            
+        }
+    }
+    
     
     var happiness: Int = 10{
         didSet{
